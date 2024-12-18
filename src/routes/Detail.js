f03a93ad135b75e278ca0e377e7d9f050c50b249
Jonghwa <whdghk3471@gmail.com>
2024-12-18 14:46:29 +0900

    modified useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,16 +6,16 @@ function Detail() {
   const [loading, setLoading] = useState(true);
   const [movieInfos, setMovieInfo] = useState();
   const { id } = useParams();
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    setMovieInfo(json.data.movie);
-    setLoading(false);
-  };
   useEffect(() => {
+    const getMovie = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovieInfo(json.data.movie);
+      setLoading(false);
+    };
     getMovie();
-  });
+  }, [id]);
   return (
     <div className={styles.container}>
       {loading ? (
